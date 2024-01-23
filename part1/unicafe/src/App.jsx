2f4e8c32abb178a17c5b9ad2e202fd800cd5dd91
lestrangeqq <lestrangeqq@gmail.com>
2024-01-23 20:20:40 +0200

    Exercise 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,25 +8,37 @@ const Button = ({onClick, text}) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
-const Stats = ({text}) => {
+const Review = ({text}) => {
   return <p>{text}</p>
+}
+
+const Stat = ({all}) => {
+  return <p>{all}</p>
 }
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
 
   const handleGood = () => {
     setGood(good + 1);
+    setAll(all+1);
+    setAverage(4/all);
   };
 
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAll(all+1)
+    setAverage(4/all);
   };
 
   const handleBad = () => {
     setBad(bad + 1);
+    setAll(all+1)
+    setAverage(4/all);
   };
 
   return (
@@ -36,9 +48,11 @@ const App = () => {
       <Button onClick={handleNeutral} text='Neutral' />
       <Button onClick={handleBad} text='Bad' />
       <Header text="Statistics" />
-      <Stats text={"Good: " + good} />
-      <Stats text={"Neutral: " + neutral} />
-      <Stats text={"Bad: " + bad} />
+      <Review text={"Good: " + good} />
+      <Review text={"Neutral: " + neutral} />
+      <Review text={"Bad: " + bad} />
+      <Stat all={"All: " + all} />
+      <Stat all={"Average: " + average} />
     </>
   );
 };
